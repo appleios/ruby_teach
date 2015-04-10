bc968f03e98481e196ce8fcc803205964e4b5d78
Aziz Latypov <vm06lau@mail.ru>
2015-04-10 11:36:57 +0500

    Update balanced.rb

--- balanced.rb
+++ balanced.rb
@@ -1,19 +1,21 @@
-def balanced(text)
-	s = []
-	text.scan(/[(){}]|\[|\]/) do |l|
-		if %w!( [ {!.include? l
-			case l 
-			when "("; s << ")"
-			when "["; s << "]"
-			when "{"; s << "}"
+class String
+	def balanced?
+		s = []
+		self.scan(/[(){}]|\[|\]/) do |l|
+			if %w!( [ {!.include? l
+				case l 
+				when "("; s << ")"
+				when "["; s << "]"
+				when "{"; s << "}"
+				end
+			elsif %w!) ] }!.include? l
+				return false if s.empty? or s.pop != l
 			end
-		elsif %w!) ] }!.include? l
-			return false if s.empty? or s.pop != l
 		end
+	
+		s.empty? # balanced if stack is empty
 	end
-
-	s.empty? # balanced if stack is empty
 end
 
-puts "balanced" if balanced(%q{this text contains [pars (()()) (and(can be) very ) complex]. But ((it) is) [balanced].})
-puts "not balanced" unless balanced(%q{this is (()))() not balanced.})
+puts "balanced" if %q{this text contains [pars (()()) (and(can be) very ) complex]. But ((it) is) [balanced].}.balanced?
+puts "not balanced" unless %q{this is (()))() not balanced.}.balanced?
